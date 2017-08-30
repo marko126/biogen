8ba8b5884cbd978ae868e3b09e379b5a7ec06927
ana.pavlovic@cubes.rs <ana.pavlovic@cubes.rs>
2017-08-30 22:32:03 +0200

    Fixed image in popup

--- views/site/_4_6_popup.php
+++ views/site/_4_6_popup.php
@@ -6,7 +6,7 @@
             <div class="popup-info-title"><?= strtoupper('viser  spor  fra ms') ?></div>
         </div>
         <div class="modal-body">
-            <div class="modal-body-left"
+            <div class="modal-body-left">
                 <ul>
                     <li>Viser immunreaktion i centralnervesystemet</li>
                     <li>Antistoffer i rygmarvsvæsken findes ikke i blodet</li>
